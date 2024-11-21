ba3b9166f2e04761f3c8f4344434c70441bb0c67
marouasaddiki <93912522+marouasaddiki@users.noreply.github.com>
2024-11-21 17:06:00 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -24,9 +24,7 @@ def meteo():
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
-  @app.route("/historigramme/")
-def histogramme():
-    return render_template("historigramme.html")
+
   
 @app.route('/extract-minutes/<date_string>')
 def extract_minutes(date_string):
